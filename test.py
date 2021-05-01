3c669d47bf123f86301209c6adbbfddba727c54a
Maneesha-Jayasundara <44173046+Maneesha-Jayasundara@users.noreply.github.com>
2021-05-01 10:38:54 +0530

    Update test.py

--- test.py
+++ test.py
@@ -17,7 +17,7 @@ stop_words = ["i", "me", "my", "myself", "we", "our", "ours", "ourselves", "you"
               "does", "did", "doing", "a", "an", "the", "and", "but", "if", "or", "because", "as", "until", "while",
               "of", "at", "by", "for", "with", "about", "against", "between", "into", "through", "during", "before",
               "after", "above", "below", "to", "from", "up", "down", "in", "out", "on", "off", "over", "under", "again",
-              "further", "then", "once", "here", "there", "when", "where", "why", "how", "all", "any", "both", "each",
+              "further", "then", "once", "here", "there", "when", "where", "why", "how", "any", "both", "each",
               "few", "more", "most", "other", "some", "such", "no", "nor", "not", "only", "own", "same", "so", "than",
               "too", "very", "s", "t", "can", "will", "just", "don", "should", "now"]
 
@@ -48,32 +48,18 @@ result = cp.parse(pos)
 print(result)
 result.draw()
 
-selective_pos = ['NN', 'JJ']
+selective_pos = ['NN', 'JJ', 'DT']
 selective_pos_words = []
 for word, tag in pos:
     if tag in selective_pos:
         selective_pos_words.append((word))
 print(selective_pos_words)
 
-emotion_list = []
-filenames = ["tables.txt", "attributes.txt", "aggregation.txt"]
-
-for filename in filenames:
- with open(filename) as infile:
-    for line in infile:
-        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
-        word, emotion = clear_line.split(':')
-
-        if word in selective_pos_words:
-            emotion_list.append(emotion)
-
-
-print(emotion_list)
 
 table = []
-attr = []
+
 aggregation_words = []
-agg = []
+
 with open('tables.txt', 'r') as file:
     for line in file:
         clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
@@ -83,30 +69,203 @@ with open('tables.txt', 'r') as file:
             table.append(emotion)
 
 
-with open('attributes.txt', 'r') as file2:
-    for line in file2:
-        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
-        word, emotion = clear_line.split(':')
 
-        if word in selective_pos_words:
-            attr.append(emotion)
+print(table)
 
-with open('aggregation.txt', 'r') as file3:
-    for line in file3:
-        clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
-        word, emotion = clear_line.split(':')
+if ' employee' in table:
+    attrEmp = []
+    with open('attributesEmp.txt', 'r') as file_Emp1:
+        for line in file_Emp1:
+             clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+             word, emotion = clear_line.split(':')
 
-        if word in selective_pos_words:
-           aggregation_words.append(emotion)
+             if word in selective_pos_words:
+              attrEmp.append(emotion)
 
 
-    if aggregation_words:
-        print("select" + " (" + ' '.join(str(x) for x in aggregation_words) + " )" + ' '.join(str(x) for x in attr))
-        print("from" + ' '.join(str(x) for x in table))
+    with open('aggregation.txt', 'r') as file_E2:
+            for line in file_E2:
+                clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+                word, emotion = clear_line.split(':')
 
-    if not aggregation_words:
-        print("select" + ' '.join(str(x) for x in attr))
-        print("from" + ' '.join(str(x) for x in table))
+                if word in selective_pos_words:
+                   aggregation_words.append(emotion)
+
+            if aggregation_words:
+                print("select" + ' '.join(str(x) for x in aggregation_words) + " (" + ' '.join(str(x) for x in attrEmp) + " )")
+                print("from" + ' '.join(str(x) for x in table))
+
+            if not aggregation_words:
+                print("select" + ' '.join(str(x) for x in attrEmp))
+                print("from" + ' '.join(str(x) for x in table))
+
+elif ' department' in table:
+    attrDept = []
+    with open('attributesDept.txt', 'r') as file_Dept1:
+        for line in file_Dept1:
+             clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+             word, emotion = clear_line.split(':')
+
+             if word in selective_pos_words:
+              attrDept.append(emotion)
 
 
+    with open('aggregation.txt', 'r') as file_Dept2:
+            for line in file_Dept2:
+                clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+                word, emotion = clear_line.split(':')
+
+                if word in selective_pos_words:
+                   aggregation_words.append(emotion)
+
+                   print(aggregation_words)
+
+            if aggregation_words:
+                print("select" + ' '.join(str(x) for x in aggregation_words) + " (" + ' '.join(str(x) for x in attrDept) + " )")
+                print("from" + ' '.join(str(x) for x in table))
+
+            if not aggregation_words:
+                print("select" + ' '.join(str(x) for x in attrDept))
+                print("from" + ' '.join(str(x) for x in table))
+
+elif ' dependent' in table:
+    attrDepend = []
+    with open('attributesDepend.txt', 'r') as file_Depend1:
+        for line in file_Depend1:
+             clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+             word, emotion = clear_line.split(':')
+
+             if word in selective_pos_words:
+              attrDepend.append(emotion)
+
+
+    with open('aggregation.txt', 'r') as file_Depend2:
+            for line in file_Depend2:
+                clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+                word, emotion = clear_line.split(':')
+
+                if word in selective_pos_words:
+                   aggregation_words.append(emotion)
+
+                   print(aggregation_words)
+
+            if aggregation_words:
+                print("select" + ' '.join(str(x) for x in aggregation_words) + " (" + ' '.join(str(x) for x in attrDepend) + " )")
+                print("from" + ' '.join(str(x) for x in table))
+
+            if not aggregation_words:
+                print("select" + ' '.join(str(x) for x in attrDepend))
+                print("from" + ' '.join(str(x) for x in table))
+
+elif ' job' in table:
+    attrJob = []
+    with open('attributesDept.txt', 'r') as file_Job1:
+        for line in file_Job1:
+             clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+             word, emotion = clear_line.split(':')
+
+             if word in selective_pos_words:
+              attrJob.append(emotion)
+
+
+    with open('aggregation.txt', 'r') as file_Job2:
+            for line in file_Job2:
+                clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+                word, emotion = clear_line.split(':')
+
+                if word in selective_pos_words:
+                   aggregation_words.append(emotion)
+
+                   print(aggregation_words)
+
+            if aggregation_words:
+                print("select" + ' '.join(str(x) for x in aggregation_words) + " (" + ' '.join(str(x) for x in attrJob) + " )")
+                print("from" + ' '.join(str(x) for x in table))
+
+            if not aggregation_words:
+                print("select" + ' '.join(str(x) for x in attrJob))
+                print("from" + ' '.join(str(x) for x in table))
+
+elif ' region' in table:
+    attrRegion = []
+    with open('attributesDept.txt', 'r') as file_Region1:
+        for line in file_Region1:
+             clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+             word, emotion = clear_line.split(':')
+
+             if word in selective_pos_words:
+              attrRegion.append(emotion)
+
+
+    with open('aggregation.txt', 'r') as file_Region2:
+            for line in file_Region2:
+                clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+                word, emotion = clear_line.split(':')
+
+                if word in selective_pos_words:
+                   aggregation_words.append(emotion)
+
+                   print(aggregation_words)
+
+            if aggregation_words:
+                print("select" + ' '.join(str(x) for x in aggregation_words) + " (" + ' '.join(str(x) for x in attrRegion) + " )")
+                print("from" + ' '.join(str(x) for x in table))
+
+            if not aggregation_words:
+                print("select" + ' '.join(str(x) for x in attrRegion))
+                print("from" + ' '.join(str(x) for x in table))
+
+elif ' country' in table:
+    attrCountry = []
+    with open('attributesDept.txt', 'r') as file_Country1:
+        for line in file_Country1:
+            clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+            word, emotion = clear_line.split(':')
+
+            if word in selective_pos_words:
+                attrCountry.append(emotion)
+
+    with open('aggregation.txt', 'r') as file_Country2:
+        for line in file_Country2:
+            clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+            word, emotion = clear_line.split(':')
+
+            if word in selective_pos_words:
+                aggregation_words.append(emotion)
+
+        if aggregation_words:
+            print("select" + ' '.join(str(x) for x in aggregation_words) + " (" + ' '.join(
+                str(x) for x in attrCountry) + " )")
+            print("from" + ' '.join(str(x) for x in table))
+
+        if not aggregation_words:
+            print("select" + ' '.join(str(x) for x in attrCountry))
+            print("from" + ' '.join(str(x) for x in table))
+
+elif ' location' in table:
+    attrLocation = []
+    with open('attributesLocation.txt', 'r') as file_Location1:
+        for line in file_Location1:
+            clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+            word, emotion = clear_line.split(':')
+
+            if word in selective_pos_words:
+                attrLocation.append(emotion)
+
+    with open('aggregation.txt', 'r') as file_Location2:
+        for line in file_Location2:
+            clear_line = line.replace("\n", '').replace(",", '').replace("'", '').strip()
+            word, emotion = clear_line.split(':')
+
+            if word in selective_pos_words:
+                aggregation_words.append(emotion)
+
+        if aggregation_words:
+            print("select" + ' '.join(str(x) for x in aggregation_words) + " (" + ' '.join(
+                str(x) for x in attrLocation) + " )" 
+            print("from" + ' '.join(str(x) for x in table))
+
+        if not aggregation_words:
+            print("select" + ' '.join(str(x) for x in attrLocation))
+            print("from" + ' '.join(str(x) for x in table))
 
